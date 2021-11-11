95666c55f304971f0e524bcf437e8dc4b88c16ce
aiden-janey <77983875+aiden-janey@users.noreply.github.com>
2021-11-10 20:10:45 -0500

    Update deployApp.js

--- deployApp.js
+++ deployApp.js
@@ -3,11 +3,14 @@ const app = express();
 var fs = require('fs');
 var port = 8080;
 
-app.get('/webpages/index.html', function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end();
+app.get('/', function (req, res) {
+  fs.readFile('./webpages/index.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 });
 
 app.listen(port, function () {
   console.log("Server up on localhost:" + port);
-})
+});
